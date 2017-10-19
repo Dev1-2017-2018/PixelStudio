b4413c3a5a75b771e635f685db51de0266126a30
Thibaut Melen <tmelendesign@gmail.com>
2017-10-19 20:34:16 +0200

    Event Mouse ++

--- js/global.js
+++ js/global.js
@@ -1,48 +1,131 @@
 window.addEventListener('DOMContentLoaded', function() {
 
 
+// INITIALISATION
+
+let  nb_column = 0;
 // CREATE DIV PAINT BLOC
 function  createSurface(line, column, size, bg_color)
 {
   let i = 1;
+  let j = 1;
   while(i <= line)
   {
-    $(".paint_div").append("<div class = \"paint_line , paint_line_num"+i+"\"></div>"); //create bloc colomn
+    $(".paint_div").append("<div class = \"paint_line\" id = \"paint_line_num"+i+"\"></div>"); //create bloc colomn
     let y = 1;
     while(y <= column)
     {
-      $(".paint_line_num"+i).append("<div class = \"paint_column , paint_column_num"+y+"\"></div>"); //create bloc colomn
-      $(".paint_line_num"+i+" div").css('background-color', "#" + bg_color);
-      $(".paint_line_num"+i+" div").css('width', size + "rem");
-      $(".paint_line_num"+i+" div").css('height', size + "rem");
+      $("#paint_line_num"+i).append("<div class = \"paint_column\" id = \""+j+"\"></div>"); //create bloc colomn
+      $("#paint_line_num"+i+" div").css('background-color', "#" + bg_color);
+      $("#paint_line_num"+i+" div").css('width', size + "rem");
+      $("#paint_line_num"+i+" div").css('height', size + "rem");
       y++;
+      j++;
     }
     i++;
   }
+  nb_column = column;
 }
 createSurface(22, 41, 2.2, 'fff');
-
-
-// ALGO
+// SELECTION DU PEN1 DEFAULT
+pen1();
+// COLOR DEFAULT
+let color = "blue";
 
 
 // EVENT CLICK IN PAINT ZONE
-$( ".paint_line" )
-  .mouseup(function() {
-    $( '.paint_column' ).unbind('mouseover');
-  })
-  .mousedown(function() {
-    $( ".paint_column" ).mouseover(function() {
-    $( this ).css('background-color', "blue");
-  })
-  .click(function() {
-    $( this ).css('background-color', "blue");
-  })
+function pen1()
+{
+  $( '.paint_column' ).unbind('mouseover');
+  $( ".paint_line" )
+    .mouseup(function() {
+      $( '.paint_column' ).unbind('mouseover');
+    })
+    .mousedown(function() {
+      $( ".paint_column" ).mouseover(function() {
+        $( this ).css('background-color', color);
+      })
+    .click(function() {
+        console.log("PEN 1");
+        $( this ).css('background-color', color);
+      })
+    })
+}
+function pen2()
+{
+  $( ".paint_line" )
+    .mouseup(function() {
+      $( '.paint_column' ).unbind('mouseover');
+    })
+    .mousedown(function() {
+      $( ".paint_column" ).mouseover(function() {
+        let i = 0;
+        while(i < 2)
+        {
+          let n_hover = (parseInt(this.id) + i) .toString();
+          console.log(n_hover);
+          $("#"+ n_hover).css('background-color', color);
+          i++;
+        }
+        i = 0;
+        while(i < 2)
+        {
+          n_hover = (parseInt(this.id) + i + nb_column) .toString();
+          console.log(n_hover);
+          $("#"+ n_hover).css('background-color', color);
+          i++;
+        }
+      })
+    .click(function() {
+        console.log("PEN 2")
+
+        let i = 0;
+        while(i < 2)
+        {
+          let n_hover = (parseInt(this.id) + i) .toString();
+          console.log(n_hover);
+          $("#"+ n_hover).css('background-color', color);
+          i++;
+        }
+        i = 0;
+        while(i < 2)
+        {
+          n_hover = (parseInt(this.id) + i + nb_column) .toString();
+          console.log(n_hover);
+          $("#"+ n_hover).css('background-color', color);
+          i++;
+        }
+
+      })
+
+    })
+}
+
+// OPTION BUTTON
+function trash(){
+  $(".paint_column").css('background-color', "white");
+}
+$( "#icon_trash" ).click(function() { trash(); })
+function rubber(){
+  color = "white";
+}
+$( "#icon_rubber" ).click(function() { rubber(); })
+
+$( "#icon_pen1" ).click(function(){
+  color = "blue";
+  pen1();
+})
+$( "#icon_pen2" ).click(function(){
+  color = "blue";
+  pen2();
 })
 
 
 
 
+
+
+// EVENT CLICK IN PAINT ZONE
 
 
 
